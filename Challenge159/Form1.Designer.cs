e3f0ee756a054543afa003d5dd9fe9c31f60d6f5
Martin Praznovsky <martin.praznovsky@gmail.com>
2016-03-02 20:36:50 +0100

    summary

---
+++ Challenge159/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Challenge159
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowserHashKiller = new System.Windows.Forms.WebBrowser();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.webBrowserChallenge = new System.Windows.Forms.WebBrowser();
+            this.textBoxLogging = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowserHashKiller
+            // 
+            this.webBrowserHashKiller.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowserHashKiller.Location = new System.Drawing.Point(16, 12);
+            this.webBrowserHashKiller.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserHashKiller.Name = "webBrowserHashKiller";
+            this.webBrowserHashKiller.Size = new System.Drawing.Size(606, 250);
+            this.webBrowserHashKiller.TabIndex = 0;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSolve.Location = new System.Drawing.Point(12, 268);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(610, 23);
+            this.buttonSolve.TabIndex = 5;
+            this.buttonSolve.Text = "Fill the CAPTCHA above (nothing else), then click here to solve the challenge";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.SolveClicked);
+            // 
+            // webBrowserChallenge
+            // 
+            this.webBrowserChallenge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowserChallenge.Location = new System.Drawing.Point(16, 297);
+            this.webBrowserChallenge.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserChallenge.Name = "webBrowserChallenge";
+            this.webBrowserChallenge.Size = new System.Drawing.Size(606, 250);
+            this.webBrowserChallenge.TabIndex = 6;
+            // 
+            // textBoxLogging
+            // 
+            this.textBoxLogging.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLogging.Location = new System.Drawing.Point(12, 553);
+            this.textBoxLogging.Multiline = true;
+            this.textBoxLogging.Name = "textBoxLogging";
+            this.textBoxLogging.ReadOnly = true;
+            this.textBoxLogging.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLogging.Size = new System.Drawing.Size(610, 145);
+            this.textBoxLogging.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 710);
+            this.Controls.Add(this.textBoxLogging);
+            this.Controls.Add(this.webBrowserChallenge);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.webBrowserHashKiller);
+            this.Name = "Form1";
+            this.Text = "Challenge 159";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserHashKiller;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.WebBrowser webBrowserChallenge;
+        private System.Windows.Forms.TextBox textBoxLogging;
+    }
+}
+
---
+++ Challenge159/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Challenge159
+{
+    public partial class Form1 : Form
+    {
+        private const string phpSessionIdName = "PHPSESSID";
+        private const string phpSessionIdValue = "ljbecct5b989gh3imvnp9otn20"; // change this one
+
+        private const string urlHashKiller = "https://hashkiller.co.uk/sha1-decrypter.aspx";
+        // "hiding" challenge url so it can not be found e.g. by google etc.
+        private string urlChallenge = "https://" + (char)114 + (char)105 + (char)110 + (char)103 + (char)122 + (char)101 + (char)114 + (char)48 + (char)116 + (char)101 + (char)97 + (char)109 + (char)46 + (char)99 + (char)111 + (char)109 + (char)47 + (char)99 + (char)104 + (char)97 + (char)108 + (char)108 + (char)101 + (char)110 + (char)103 + (char)101 + (char)115 + "/159";
+
+        private const string captchaInputId = "content1_txtCaptcha";
+        private const string submitButtonId = "content1_btnSubmit";
+        private const string textAreaHashId = "content1_txtInput";
+        private const string textAreaBrokenId = "content1_lblResults";
+        private const string brokenSpanClass = "text-green";
+
+        private const string beginHash = "----- BEGIN HASH -----";
+        private const string endHash = "----- END HASH -----";
+        private const string hashDivClass = "message";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // for setting the cookie
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool InternetSetCookie(string lpszUrlName, string lpszCookieName, string lpszCookieData);
+
+        private void FormLoad(object sender, EventArgs e)
+        {          
+            // on form load do load also the haskiller page            
+            webBrowserHashKiller.Url = new Uri(urlHashKiller);
+
+            // prepare the cookie for challenge page
+            Cookie cookie = new Cookie(phpSessionIdName, phpSessionIdValue);
+            InternetSetCookie(urlChallenge, null, cookie.ToString() + "; expires = Sun, 05-Jan-2020 00:00:00 GMT");
+        }        
+
+        private void SolveClicked(object sender, EventArgs e)
+        {
+            AddLogLine("Solve button clicked");
+            AddLogLine($"Using {phpSessionIdName}={phpSessionIdValue}");
+            AddLogLine($"Using CAPTCHA: {webBrowserHashKiller.Document.GetElementById(captchaInputId).GetAttribute("value")}");
+
+            webBrowserChallenge.DocumentCompleted += ChallengePageLoaded;
+            webBrowserChallenge.Url = new Uri(urlChallenge);
+        }
+
+        private void ChallengePageLoaded(object sender, EventArgs e)
+        {
+            webBrowserChallenge.DocumentCompleted -= ChallengePageLoaded;
+
+            var hash = string.Empty;
+
+            foreach (HtmlElement div in webBrowserChallenge.Document.GetElementsByTagName("div"))
+            {
+                if (div.GetAttribute("classname") == hashDivClass)
+                {
+                    hash = div.InnerText.Replace(beginHash, string.Empty).Replace(endHash, string.Empty).Trim();
+                    AddLogLine($"Using HASH: {hash}");
+                }
+            }
+
+            webBrowserHashKiller.Document.GetElementById(textAreaHashId).SetAttribute("value", hash);
+            webBrowserHashKiller.Document.GetElementById(submitButtonId).InvokeMember("click");
+
+            // fucking complicated to check for change after POST within WebBrowser (hash killer page uses aspx...)
+            // just set a 1 second interval and then parse the broken hash value
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(BrokenHashPageLoaded);
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void BrokenHashPageLoaded(object sender, EventArgs e )
+        {
+            var timer = (Timer)sender;
+            timer.Stop();
+
+            var broken = string.Empty;
+
+            foreach (HtmlElement span in webBrowserHashKiller.Document.GetElementsByTagName("span"))
+            {
+                if (span.GetAttribute("classname") == brokenSpanClass)
+                {
+                    broken = span.InnerText;
+                    AddLogLine($"Broken hash: {broken}");
+                }
+            }
+
+            webBrowserChallenge.Url = new Uri(urlChallenge + "/" + broken);
+            AddLogLine("Check the bottom page for flag.");
+        }
+
+        private void AddLogLine(string message)
+        {
+            textBoxLogging.AppendText(message + Environment.NewLine);
+            textBoxLogging.ScrollToCaret();
+        }
+    }
+}
